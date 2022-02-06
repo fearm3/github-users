ff730a18c4065b0678d7e0c241d123087f5ba60f
fearm3 <ibraaa1628@gmail.com>
2022-02-06 17:11:58 +0300

    add FusionCharts

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -1,9 +1,31 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+  console.log(repos);
+  const chartData = [
+    {
+      label: "HTML",
+      value: "13",
+    },
+    {
+      label: "CSS",
+      value: "150",
+    },
+    {
+      label: "Javascript",
+      value: "80",
+    },
+  ];
+  return (
+    <section className="section">
+      <Wrapper section-center>
+        <ExampleChart data={chartData} />
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
