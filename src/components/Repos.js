4bf5ea8bbf1ef01f30efc724063f74b4daa50564
fearm3 <ibraaa1628@gmail.com>
2022-02-07 19:47:10 +0300

    add most Used language

--- src/components/Repos.js
+++ src/components/Repos.js
@@ -4,7 +4,26 @@ import { GithubContext } from "../context/context";
 import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
   const { repos } = React.useContext(GithubContext);
-  console.log(repos);
+
+  let languages = repos.reduce((total, item) => {
+    const { language } = item;
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1 };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+      };
+    }
+    return total;
+  }, {});
+  languages = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+
   const chartData = [
     {
       label: "HTML",
@@ -22,7 +41,8 @@ const Repos = () => {
   return (
     <section className="section">
       <Wrapper section-center>
-        <ExampleChart data={chartData} />
+        <Pie3D data={languages} />
+        {/* <ExampleChart data={chartData} /> */}
       </Wrapper>
     </section>
   );
