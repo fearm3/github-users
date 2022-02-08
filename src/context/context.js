0dbfa8bc10a7e83bdcbebf4582d0a7440309649a
fearm3 <ibraaa1628@gmail.com>
2022-02-08 17:03:07 +0300

    add Repos and followers

--- src/context/context.js
+++ src/context/context.js
@@ -29,7 +29,21 @@ const GithubProvider = ({ children }) => {
     );
     if (response) {
       setGithubUser(response.data);
+      const { login, followers_url } = response.data;
+      //repos
+      axios(`${rootURL}/users/${login}/repos?per_page=100`).then((response) =>
+        setRepos(response.data)
+      );
+      //followers
+      axios(`${followers_url}?per_page=100`).then((response) =>
+        setFollowers(response.data)
+      );
+
       //more logic here
+      //repos
+      //https://api.github.com/users/fearm3/repos?per_page=100
+      //followers
+      //https://api.github.com/users/fearm3/followers
     } else {
       toggleError(true, "there is no user with that username");
     }
